82cc4f4c79a41db398f9986db2f439fcfbea35d4
mohmad gamal <48861944+Mohmad012@users.noreply.github.com>
2022-11-05 18:02:56 +0200

    update next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,5 +9,16 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
       authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      if (account.provider === 'google' &&
+          profile.verified_email === true &&
+          profile.email.endsWith('@gmail.com')) {
+        return true
+      } else {
+        return false
+      }
+    },
+  }
 })
